19aba5bc38bf92d9cdd68d9fdc77fe5b2908e4e8
abood1509 <abdbinm55@gmail.com>
2025-02-07 21:46:40 +0300

    new: new custome cursor was added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,27 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { PlayProvider } from "./contexts/Play";
 import "./index.css";
+import AnimatedCursor from "react-animated-cursor"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PlayProvider>
       <App />
+      <AnimatedCursor
+        innerSize={20}
+        outerSize={40}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+      />
     </PlayProvider>
   </React.StrictMode>
 );
