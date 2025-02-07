39bd110f974d083c219b77f9a82385bcf51aefee
abood1509 <abdbinm55@gmail.com>
2025-02-07 20:23:34 +0300

    new: new gltfjsx object(some not used)

---
+++ src/components/Star.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Stars } from "@react-three/drei";
+import "../index.css"
+export const Star = () => {
+    return (
+        <Stars
+            radius={100} // Radius of the sphere
+            depth={10} // Depth of area where stars should be
+            count={5000} // Number of stars
+            factor={7} // Size factor
+            saturation={0} // Saturation of stars
+            fade // Faded edges
+        />
+    );
+};
