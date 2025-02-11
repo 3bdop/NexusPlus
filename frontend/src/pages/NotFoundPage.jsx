bd4a2da1084d71a8728af78418e212bbfcbb3ba7
abood1509 <abdbinm55@gmail.com>
2025-02-11 17:13:03 +0300

    changed path

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFoundpage() {
+    return (
+        <div>
+            <h1>It worked on my machine</h1>
+            <p>But apparently not on the world wide web. Sorry about that! 🌍</p>
+        </div>
+    )
+}
