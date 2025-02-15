6c8f1a7d3c365fe06eb23239219516b0dea7e783
abood1509 <abdbinm55@gmail.com>
2025-02-15 18:22:11 +0300

    fixed the plane issue after skip button clicked

--- frontend/src/components/Overlay.jsx
+++ frontend/src/components/Overlay.jsx
@@ -4,12 +4,9 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Login from "../pages/Login";
 export const Overlay = () => {
-  const navigate = useNavigate()
-  const handleLoginClick = () => {
-    navigate("/login")
-  }
   const { progress } = useProgress();
   const { play, end, setPlay, hasScroll, setEnd } = usePlay();
+  console.log(end)
   return (
     <div
       className={`overlay ${play ? "overlay--disable" : ""}
@@ -34,10 +31,11 @@ export const Overlay = () => {
             onClick={() => {
               setPlay(true);
             }}
+            disabled={end}
           >
             Explore
           </button>
-          <button className="cta" onClick={() => { setEnd(true) }}>
+          <button className="cta" onClick={() => { setEnd(true) }} disabled={end}>
             <span className="hover-underline-animation"> Skip </span>
             <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width={30} height={10} viewBox="0 0 46 16">
               <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)" />
