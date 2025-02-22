d3a59260fba8e2658d5abe1de26ef6e1a089f844
abood1509 <abdbinm55@gmail.com>
2025-02-22 14:16:56 +0300

    add another origin in CORS for test

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,24 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cookieParser()); // Add this before your routes
-app.use(cors({
-    origin: 'http://localhost:5173', // Your frontend URL
-    credentials: true // Important for cookies
-})); app.use(express.json());
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://192.168.118.115:5173'
+];
+
+app.use(cors(
+    {
+        origin: function (origin, callback) {
+            // Allow requests with no origin (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+        credentials: true // Important for cookies
+    })); app.use(express.json());
 app.use("/", records);
 app.use("/webgl", WebGLBuild)
 
