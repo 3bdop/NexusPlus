82b3764274e08a0767f68fa18545b2b0f76a6813
abood1509 <abdbinm55@gmail.com>
2025-02-24 22:26:06 +0300

    update style

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function NotFoundpage() {
     return (
@@ -13,9 +13,9 @@ export default function NotFoundpage() {
             background: 'linear-gradient(5deg, #1D1C1CFF 0%, #2516ADFF 100%)',
             position: 'relative'
         }}>
-            <h1 style={{ color: 'snow' }}>It worked on my machine</h1>
-            <p style={{ color: 'snow' }}>But apparently not on the world wide web. Sorry about that! 🌍</p>
-            <Link to={"/home"}>Back to home</Link>
+            <h1 style={{ color: 'snow', fontFamily: "system-ui" }}>It worked on my machine🤔</h1>
+            <p style={{ color: 'snow', fontFamily: "system-ui" }}>But apparently not on the world wide web. Sorry about that! 🌍</p>
+            <NavLink to={"/home"} style={{ fontFamily: "system-ui" }} >Back to home</NavLink>
         </div>
     )
 }
