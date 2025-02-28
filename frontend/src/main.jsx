d0c4d1f2b256d147befbc1d0d7e434771d7612b3
abood1509 <abdbinm55@gmail.com>
2025-03-01 00:50:39 +0300

    update routes to work with new dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,81 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import App from "./App";
+// import { PlayProvider } from "./contexts/Play";
+// import "./index.css";
+// import AnimatedCursor from "react-animated-cursor"
+
+// import NotFoundPage from "./pages/NotFoundPage.jsx"
+// import Home from "./pages/Home.jsx";
+// import Login from "./pages/Login.jsx";
+// import ProtectedRoute from "./elements/ProtectedRoute.jsx";
+// import AvatarCreation from "./pages/AvatarCreation.jsx";
+// import CareerFair from "./pages/CareerFair.jsx";
+// import DashboardLayoutBasic from "./pages/DashboardLayoutBasic.jsx"
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: (
+//       <PlayProvider>
+//         <App />
+//       </PlayProvider>
+//     ),
+//     errorElement: <NotFoundPage />,
+//   },
+//   {
+//     path: '/home',
+//     element: (
+//       <ProtectedRoute>
+//         <Home />
+//       </ProtectedRoute>
+//     ),
+//     errorElement: <NotFoundPage />
+//   },
+//   {
+//     path: '/avatar-creation',
+//     element: (
+//       <ProtectedRoute>
+//         <AvatarCreation />
+//       </ProtectedRoute>
+//     ),
+//     errorElement: <NotFoundPage />
+//   },
+//   {
+//     path: '/career-fair',
+//     element: (
+//       <ProtectedRoute>
+//         <CareerFair />
+//       </ProtectedRoute>
+//     ),
+//     errorElement: <NotFoundPage />
+//   },
+//   {
+//     path: '/test',
+//     element: <DashboardLayoutBasic />
+//   }
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from "./App";
 import { PlayProvider } from "./contexts/Play";
 import "./index.css";
-import AnimatedCursor from "react-animated-cursor"
-
-import NotFoundPage from "./pages/NotFoundPage.jsx"
-import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Home from "./pages/Home.jsx";
 import AvatarCreation from "./pages/AvatarCreation.jsx";
 import CareerFair from "./pages/CareerFair.jsx";
-import DashboardLayoutBasic from "./pages/DashboardLayoutBasic.jsx"
+// import RecommendedJobs from "./pages/RecommendedJobs.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import Dashboarddd from "./pages/Dashboarddd.jsx";
 
 const router = createBrowserRouter([
   {
@@ -31,16 +94,22 @@ const router = createBrowserRouter([
         <Home />
       </ProtectedRoute>
     ),
-    errorElement: <NotFoundPage />
-  },
-  {
-    path: '/avatar-creation',
-    element: (
-      <ProtectedRoute>
-        <AvatarCreation />
-      </ProtectedRoute>
-    ),
-    errorElement: <NotFoundPage />
+    children: [
+      {
+        path: '',
+        element: <Dashboarddd />,
+        children: [
+          // {
+          //   path: 'career-fair',
+          //   element: <CareerFair />,
+          // },
+          {
+            path: 'avatar-creation',
+            element: <AvatarCreation />,
+          },
+        ]
+      }
+    ],
   },
   {
     path: '/career-fair',
@@ -50,10 +119,6 @@ const router = createBrowserRouter([
       </ProtectedRoute>
     ),
     errorElement: <NotFoundPage />
-  },
-  {
-    path: '/test',
-    element: <DashboardLayoutBasic />
   }
 ]);
 
