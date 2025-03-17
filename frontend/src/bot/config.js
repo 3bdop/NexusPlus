cc7967774fa80cd869bb9244af322eba607e5ec8
abood1509 <abdbinm55@gmail.com>
2025-03-17 17:15:17 +0300

    remove comments

--- frontend/src/bot/config.js
+++ frontend/src/bot/config.js
@@ -7,14 +7,6 @@ const botName = 'Daleel';
 const config = {
     initialMessages: [createChatBotMessage(`Hey 👋, I'm ${botName}`)],
     botName: botName,
-    // customStyles: {
-    //     botMessageBox: {
-    //         backgroundColor: '#2436A7FF',
-    //     },
-    //     chatButton: {
-    //         backgroundColor: '#5C98CCFF',
-    //     },
-    // },
 };
 
 export default config;
