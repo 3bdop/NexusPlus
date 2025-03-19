094062d095c134fd1c9810e024fc69a93f66d2c5
abood1509 <abdbinm55@gmail.com>
2025-03-20 01:42:33 +0300

    chore

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -3,16 +3,18 @@ import { NavLink } from 'react-router-dom'
 
 export default function NotFoundpage() {
     return (
-        <div style={{
-            height: '100vh',
-            width: '100vw',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            background: 'linear-gradient(5deg, #1D1C1CFF 0%, #2516ADFF 100%)',
-            position: 'relative'
-        }}>
+        <div
+            style={{
+                height: '100vh',
+                width: '100vw',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                background: 'linear-gradient(5deg, #1D1C1CFF 0%, #2516ADFF 100%)',
+                position: 'relative'
+            }}
+        >
             <h1 style={{ color: 'snow', fontFamily: "system-ui" }}>
                 It worked on my machine
                 <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f914/512.gif" alt="🤔" width="50" height="50" align="center" />
