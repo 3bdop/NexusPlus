c1c825f5a166949dee47945641de4feeb741abdc
abood1509 <abdbinm55@gmail.com>
2025-03-21 05:14:54 +0300

    add custom colors to Tailwind config and integrate image optimizer plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Add custom colors used in the card
+        dark: {
+          900: "#0E0E10",
+          800: "#1d1c20",
+          700: "#323238",
+        },
+      },
+    },
   },
   plugins: [],
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import imageOptimizer from 'vite-plugin-image-optimizer'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  imageOptimizer({})
+  ],
 })
