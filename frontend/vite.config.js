b19dde548b25a77b9b1365455048d500b918b5eb
abood1509 <abdbinm55@gmail.com>
2025-03-21 05:26:19 +0300

    remove unused vite-plugin-image-optimizer dependency and related config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import imageOptimizer from 'vite-plugin-image-optimizer'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  imageOptimizer({})
-  ],
+  plugins: [react()],
 })
