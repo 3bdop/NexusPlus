c2a9cafb872587543bb323c3009420fb38729f43
abood1509 <abdbinm55@gmail.com>
2025-03-28 14:09:31 +0300

    feat: disable Tailwind CSS preflight for custom styling flexibility

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: 'tw-',
+  corePlugins: {
+    preflight: false,
+  },
   content: [
     // "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
