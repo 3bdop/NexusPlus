0a7971030f3401edb101f4d14c2db4a1f2219bb3
Abdulrahman <77641682+3bdop@users.noreply.github.com>
2025-04-07 20:06:38 +0300

    Update run_employer.py

--- backend/recommendation/src/api/run_employer.py
+++ backend/recommendation/src/api/run_employer.py
@@ -4,7 +4,7 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8002))
     uvicorn.run(
-        "src.api.employer_endpoints:app",
+        "src.api.candidate_main:app",
         host="0.0.0.0",
         port=port,
     )
