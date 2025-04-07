b0793b5f6f49ccab05cdf18fa5b92575ddf7e10b
abood1509 <abdbinm55@gmail.com>
2025-04-07 03:36:13 +0300

    add axios import

--- frontend/src/pages/JobApplications.jsx
+++ frontend/src/pages/JobApplications.jsx
@@ -17,7 +17,7 @@ import { useState, useEffect } from 'react';
 import { Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-import { pdfjs } from 'react-pdf';
+import axios from 'axios'
 import { apiClient } from '../api/client';
 
 export default function CompanyJobs() {
