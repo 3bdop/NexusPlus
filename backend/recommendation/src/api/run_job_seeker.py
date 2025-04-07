10d7e431e141b460de29e6d2c6b5a305fb9ee642
Abdulrahman <77641682+3bdop@users.noreply.github.com>
2025-04-07 20:27:47 +0300

    Update run_job_seeker.py

--- backend/recommendation/src/api/run_job_seeker.py
+++ backend/recommendation/src/api/run_job_seeker.py
@@ -2,9 +2,8 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8001))
     uvicorn.run(
         "src.api.main:app",
         host="0.0.0.0",
-        port=port,
+        port=8001,
     )
